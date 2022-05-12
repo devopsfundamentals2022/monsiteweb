1a142991241c39fd6c38345488d05d54776e7382
Kev <kevin.nimsgern@cfl.lu>
2022-05-12 12:07:48 +0000

    location change

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -4,7 +4,7 @@ variable "resource_group_name_prefix" {
 }
 
 variable "resource_group_location" {
-  default       = "South_Central_US"
+  default       = "southeastasia"
   description   = "Location of the resource group."
 }
 
