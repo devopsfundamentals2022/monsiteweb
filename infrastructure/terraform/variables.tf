cfcd8ef8fdffa798ff538b5b7eb1f701dd08b631
dimitri <dimitri.libessart@cfl.lu>
2022-05-12 12:02:22 +0000

    central

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -4,7 +4,7 @@ variable "resource_group_name_prefix" {
 }
 
 variable "resource_group_location" {
-  default       = "South Central US"
+  default       = "Central US"
   description   = "Location of the resource group."
 }
 
