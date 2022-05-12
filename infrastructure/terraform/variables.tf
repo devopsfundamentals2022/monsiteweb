411a584d976b735dbfbb2a6625abd7ec5358ced8
unknown <student@gmail.com>
2022-05-12 13:37:38 +0200

    Terraform

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name_prefix" {
+  default       = "pascal"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "resource_group_location" {
+  default       = "eastus"
+  description   = "Location of the resource group."
+}
+
+variable "resource_lab_name" {
+  default       = "pascal"
+  description   = "Name of student"
+}
